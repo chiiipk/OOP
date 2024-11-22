8d34e3889a80dda5789200ec65766c3a62f3c0de
pikachu <phamkhanhchi0406@gmail.com>
2024-11-22 21:00:35 +0700

    feature/print-cart

---
+++ Lab03/AimProject/src/StoreTest.java
@@ -0,0 +1,16 @@
+
+public class StoreTest {
+	public static void main(String[] args) {
+		Store store = new Store();
+		
+		store.addDVD("Clueless");
+		store.addDVD("Legally Blonde");
+		store.addDVD("Doraemon");
+		store.displayDVDs();
+		
+		
+		store.removeDVD("Clueless");
+		store.removeDVD("Doraemon");
+		store.displayDVDs();
+	}
+}
